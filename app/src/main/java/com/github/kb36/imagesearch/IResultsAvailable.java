60bdfb9f2312654d22f59e9189f9356b7d8174d3
kb36 <nagarjuna.t2@gmail.com>
2015-10-28 01:11:42 -0400

    Image Search Android application
    Image Search android application using google imagesearch api.

---
+++ app/src/main/java/com/github/kb36/imagesearch/IResultsAvailable.java
@@ -0,0 +1,8 @@
+package com.github.kb36.imagesearch;
+
+/**
+ * Created by nagarjuna.t1 on 10/23/2015.
+ */
+public interface IResultsAvailable {
+    public void onResultsAvailable(QueryResult results);
+}
